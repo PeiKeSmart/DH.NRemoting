a094065d82e4ecfe73fec3cdd6029c2d60a60603
猿人易 <qcjxberin@outlook.com>
2024-08-20 13:54:09 +0800

    重命名 TestCaseOrderer 命名空间
    将 `TestCaseOrderer` 的命名空间从 `"NewLife.UnitTest"` 修改为 `"DH.UnitTest"`。

--- XUnitTest/ApiTest.cs
+++ XUnitTest/ApiTest.cs
@@ -12,7 +12,7 @@ using Xunit;
 
 namespace XUnitTest.Remoting;
 
-[TestCaseOrderer("NewLife.UnitTest.DefaultOrderer", "NewLife.UnitTest")]
+[TestCaseOrderer("NewLife.UnitTest.DefaultOrderer", "DH.UnitTest")]
 public class ApiTest : DisposeBase
 {
     private static ApiServer _Server;
